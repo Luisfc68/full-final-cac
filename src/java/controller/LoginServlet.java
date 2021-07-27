85c6878714f3e1d44c7d7afaceb847060dac39ea
Luisfc68 <Luisfc68@gmail.com>
2021-07-27 17:29:04 -0300

    Agrega LoginServlet

---
+++ src/java/controller/LoginServlet.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import excepciones.PersistenciaException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Usuario;
+import persistencia.UsuarioDAO;
+import persistencia.UsuarioDaoImp;
+
+/**
+ *
+ * @author Luis
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try{
+            
+            String nombreUsuario = request.getParameter("nombre-usuario");
+            String clave = request.getParameter("clave");
+            if(nombreUsuario != null && clave != null){
+                
+                Usuario usuario = new UsuarioDaoImp().getByName(nombreUsuario);
+                HttpSession session = request.getSession();
+                Boolean errorClave = false;
+                
+                if(usuario != null && usuario.getClave().equals(clave)){
+                    
+                    session.setAttribute("usuario", usuario);
+                    response.sendRedirect("pedido.jsp");
+                    
+                }else{
+                    
+                    errorClave = true;
+                    response.sendRedirect("login.jsp");
+                    
+                }
+                
+                session.setAttribute("errorClave",errorClave);
+            }
+            
+            
+            
+        }catch(PersistenciaException e){
+            System.err.println(e.getMessage());
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("login.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
